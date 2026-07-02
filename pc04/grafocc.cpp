6bbd43d8ffb38c0af559c6e29b92ca909baedbf5
Opiumxth <aki.chainsaw24@gmail.com>
2026-07-02 14:01:17 -0500

    Solucion de la practica calificada 04, grafos, matriz de adyacencia, lista de adyacencia y dfs

---
+++ pc04/grafocc.cpp
@@ -0,0 +1,304 @@
+/*
+* nomPrograma: grafocc.cpp
+* descripcion: opraciones basicas de grafo matriz de adyacencia
+*
+*
+*
+*
+* autor:
+* fecha:
+* Version 1
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+//#include <conio.h> // libreria no standard
+#include <string.h>
+#include "Grafo.h"
+
+using namespace std;
+
+// ======================================================
+// RETO 1: GRAFO: MATRIZ DE ADYACENCIA ESTÁTICA
+// ------------------------------------------------------
+// Crear Grafo matriz
+void createGraphMatrix(GraphMatrix *g)
+{
+    //g = NULL; // Porque si,  no?
+    g->numVertices = -1;
+}
+// Leer los elementos de la matriz
+void readGraphMatrix(GraphMatrix *g)
+{
+    int i, j, nv;
+    printf("\nNumero vertices Matriz Adyacencia: ");
+    scanf("%d", &nv);
+    //if (g == NULL || vertices > MAX20 || vertices <= 0) {
+    if (g == NULL || nv > MAX20 || nv <= 0) {
+        printf("%d: Numero de vertices fuera de rango...\n", nv);
+        return;
+    }
+    else {
+        // Inicializar la matriz.
+        // Recuerda: la diagonal principal debe ser 0 (costo a sí mismo).
+        // El resto de celdas deben inicializarse con la constante INF.
+      g->numVertices = nv;
+
+        for(i=0;i<nv;i++)
+        {
+            for(j=0;j<nv;j++)
+            {
+                if(i==j)
+                    g->matriz[i][j]=0;
+                else
+                    g->matriz[i][j]=INF;
+            }
+        }
+
+    }
+}
+
+// Addiconar aristas al grafo
+void insertEdgeMatrix(GraphMatrix* g, int orig, int dest, int weight)
+{
+    if (g == NULL) {
+        return;
+    }
+    // Validar los límites aristas/Edges
+    // insertar la arista ponderada.
+    // Recuerda que este grafo debe ser NO DIRIGIDO (simétrico).
+
+   if(orig < 0 || orig >= g->numVertices) return;
+   if(dest < 0 || dest >= g->numVertices) return;
+    g->matriz[orig][dest] = weight;
+    g->matriz[dest][orig] = weight;
+ }
+
+// Mostrar matriz de adyaencia
+void showGraphMatrix(GraphMatrix* g)
+{
+    int i, j;
+    char inf[] = "INF";
+    char spc[] = " ";
+    if (g == NULL) {
+        return;
+    }
+    printf("\n--- MATRIZ DE ADYACENCIA ---\n");
+    printf("%4s", spc);
+    for (i = 0; i < g->numVertices; i++) {
+        printf("%4d", i);
+    }
+    printf("\n\n");
+
+    for (i = 0; i < g->numVertices; i++) {
+        printf("%4d ", i);
+        for (j = 0; j < g->numVertices; j++) {
+            if (g->matriz[i][j] == -1) {
+                printf("%4s", inf);
+            } else {
+                printf("%4d", g->matriz[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+// ======================================================
+// RETO 2: GRAFO: LISTA DE ADYACENCIA DINÁMICA
+// ------------------------------------------------------
+
+//Crear grafo
+void createGraphList(GraphList *g)
+{
+    g = NULL;
+}
+
+//Leer elementos para el grafo
+GraphList* readGraphList(GraphList *g)
+{
+    int i, nv;
+
+    printf("\nNumero vertices Lista Adyacencia:  ");
+    scanf("%d", &nv);
+
+    if (nv <= 0) {
+        return NULL;
+    }
+    else {
+        // Asignar memoria para la estructura principal GraphList g
+        // Asignar numero de vertices al grafo
+        // Crear un arreglo de listas de adyacencia
+        // Iniciliar cada elementos de la lista como vacia o sea ?
+
+        g = (GraphList*)malloc(sizeof(GraphList));
+        g->numVertices = nv;
+
+        g->adjLists = (Node**)malloc(nv * sizeof(Node*));
+
+        for(i = 0; i < nv; i++)
+        {
+            g->adjLists[i] = NULL;
+        }
+
+        return g;
+
+
+    }
+}
+
+//Adicionar aristas al grafo
+void insertEdgeList(GraphList *g, int orig, int dest)
+{
+    if (g == NULL) {
+        return;
+    }
+    if (orig < 0 || orig >= g->numVertices || dest < 0 || dest >= g->numVertices) {
+        return;
+    }
+    else {
+        // Caso Grafo no dirigido
+        // Crear el metodo crearNodo(). Referencia y asignar vertice orgen
+        // Insertar el nodo AL INICIO de la lista enlazada apuntada por
+        // g->adjLists[destino] (Costo O(1)).
+        // Caso Grafo dirigido ?
+
+        /*
+        // Para grafo no dirigido
+        Node *neo = (Node*)malloc(sizeof(Node));
+        neo->targetVertex = orig;
+        neo->next = NULL;
+        neo->next = g->adjLists[dest];
+        g->adjLists[dest] = neo;
+        */
+
+        // Para grafo dirigido
+        Node *neo = (Node*)malloc(sizeof(Node));
+        neo->targetVertex = dest;
+        neo->next = g->adjLists[orig];
+        g->adjLists[orig] = neo;
+
+    }
+}
+
+// Mostrar grafo
+void showGraphList(GraphList *g)
+{
+    int i;
+    Node *edg;
+    printf("\n");
+    line2();
+    printf(" G R A F O S   Lista de Adyacencia\n");
+    line1();
+
+    // Debe recorrer el vector de vertices que apuntan a nodos arista
+    // y debe configurar la lista de adyacencia
+    // No olvide las consistencias del caso
+
+   if (g == NULL) return;
+   // Ahora mostramos el grafo con el formato |Vertice|->
+   for(i = 0; i < g->numVertices; i++){
+      printf("|%3d|-> ", i);
+
+      edg = g->adjLists[i];
+
+      while(edg != NULL){
+         printf("%3d--> ", edg->targetVertex);
+         edg = edg->next;
+      }
+      printf("NULL\n");
+   }
+   line1();
+}
+
+// Liberar memoria del grafo
+void freeGraphList(GraphList *g)
+{
+    int i;
+    Node *actual, *siguiente;
+    if (g == NULL) {
+        return;
+    }
+    else {
+        // Recorrer el arreglo g->adjLists.
+        // Para cada posición, liberar iterativa o recursivamente todos
+        // los nodos de la lista enlazada.
+        // Finalmente, liberar el arreglo g->adjLists y la estructura principal g.
+
+      for(i = 0; i < g->numVertices; i++){
+         actual = g->adjLists[i];
+         // Se libera uno por uno usando free
+         while(actual != NULL){
+            siguiente = actual->next;
+            free(actual);
+            actual = siguiente;
+         }
+      }
+      free(g->adjLists);
+      free(g);
+
+      printf("\nSe libero la memoria del grafo lista.\n");
+   }
+}
+
+// ======================================================
+// RETO 3: RECORRIDOS (DFS) Recursivo
+// ------------------------------------------------------
+
+// 3. RECORRIDO DFS (Función Auxiliar Recursiva)
+void recursiveDFS(GraphList *g, int vertex, int *visited)
+{
+    // Implementación del recorrido en profundidad recursivo.
+    // 1. Marcar startVertex como visitado (1).
+    // 2. Imprimir el vértice actual para la traza.
+    // 3. Obtener el primer vecino de la lista apuntada por g->adjLists[startVertex].
+    // 4. Recorrer la lista enlazada de vecinos: si un vecino no ha sido visitado (0),
+    //    llamar recursivamente a DFS pasándole ese vecino.
+   visited[vertex] = 1;
+
+   printf("%d ", vertex);
+   Node *aux = g->adjLists[vertex];
+
+   while(aux != NULL){
+      if(!visited[aux->targetVertex]){
+         recursiveDFS(g, aux->targetVertex, visited);
+      }
+      aux = aux->next;
+   }
+
+}
+
+// Función principal para iniciar el DFS
+void DFS(GraphList* g, int startVertex)
+{
+
+    if (g == NULL || startVertex < 0 || startVertex >= g->numVertices) {
+        return;
+    }
+
+    printf("\nRecorrido DFS desde vértice %d: ", startVertex);
+    int* visited = (int*)calloc(g->numVertices, sizeof(int));
+
+    recursiveDFS(g, startVertex, visited);
+
+    printf("\n");
+    free(visited);
+}
+
+void line1()
+{
+    int i, n=40;
+    for(i=0;i<n; i=i+1){
+        printf("-");
+    }
+    printf("\n");
+}
+
+void line2()
+{
+    int i, n=40;
+    for(i=0;i<n; i=i+1){
+        printf("=");
+    }
+    printf("\n");
+}
---
+++ pc04/maincc.cpp
@@ -0,0 +1,207 @@
+/*
+* nomPrograma: maincc.cpp
+* descripcion: Programa principal del Grafo
+*
+*                   PC04 GRAFOS Matriz y Lista Adyacencia
+*                         Recorrido DFS Recursivo
+*
+* 0. Se tiene el "esqueleto" de la aplicacion Matriz y Lista de adyacencia.
+* 1. Analice el TAD Grafo donde se declara las estructuras necesarias y la
+*    especificacion de las operaciones.
+* 2. Analice bien las implemntaciones de la matriz y de lista de adyacencia.
+* 3. Analice bien las pautas que se dan.
+* 4. Los RETOS valen 4 putos cada uno, el resto de puntanje se obtiene de las
+*    deemas implementaciones para que la aplicacion funcione adecuandamente.,
+*
+*    IMPORTANTE:
+*    Rotule adecuadamente la hoja cuadriculada de la PC04 Grafos, En el
+*    debe construir/crear el grafo con minimo de 7 vertices  tanto para la matriz
+*    como lista de adyacencia con la relaciones y pesos adecuados
+*    MATRIZ DE AYACENCIA:
+*    1. Dibuje el grafo con sus respectivas relaciones.
+*    2. Dibuje la matriz de adyacencia
+*    LISTA DE ADYACENCIA:
+*    3. Dibuje el grafo con sus respectivas relaciones
+*    4. Dibuje la Lista de ayacencia
+*    5, Muestre el recorrido DFS del grafo
+*
+*    NO COPIE EL EJEMPLO que se da como salida de la aplicacion, Cree su
+*    propio ejemplo que va a cargar a la aplicacion.
+*    Cree una carpeta PC04 PaternoMaternoNombre, lo comprime y lo envia al
+*    correo. La hoja cuadriculada contiene los datos solicitados de la
+*    aplicacion de la PC04.
+*
+*
+*
+* autor: FLORES HOYOS, Mathias
+* fecha: 02-07-2026
+* Version 1
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+//#include <conio.h> // libreria no standard
+#include <string.h>
+#include "Grafo.h"
+#include "grafocc.cpp"
+
+using namespace std;
+
+/*
+int main()
+{
+
+    printf("---> LABORATORIO DE GRAFOS \n");
+    // ======================================================
+    // Prueba Reto 1: GRAFO: Matriz de Adyacencia
+    // ------------------------------------------------------
+    GraphMatrix gm1, gm2, gm3;
+    createGraphMatrix(&gm1);
+
+
+    insertEdgeMatrix(&gm1, 0, 1, 5);
+    // ...
+
+    // ======================================================
+    // Prueba Reto 2 y 3: GRAFO: Lista de Adyacencia y DFS
+    // ------------------------------------------------------
+
+    GraphList *gl1, *gl2, *gl3;
+    createGraphList(gl1);
+    gl1 = readGraphList(gl1);
+    if (gl1 != NULL) {
+        // Construcción del grafo dirigido de prueba obligatoria
+        insertEdgeList(gl1, 0, 1);
+        // ...
+        // Ejecución del recorrido
+        printf("Recorrido en profunidad...\n");
+        //DFS(gl1, 0);
+
+        // Liberación absoluta de memoria
+        freeGraphList(gl1);
+    }
+
+
+    //system("pause");
+    return 0;
+}
+*/
+
+
+
+// Solucion de la PC04
+
+int main()
+{
+   printf("---> LABORATORIO DE GRAFOS\n");
+
+   // MATRIZ DE ADYACENCIA
+
+   GraphMatrix gm1;
+
+   createGraphMatrix(&gm1);
+
+   readGraphMatrix(&gm1);
+
+   showGraphMatrix(&gm1);
+
+   // Llenando con los valores
+   // Revisar la hoja para ver el grafo con los pesos
+   insertEdgeMatrix(&gm1,0,1,5);
+   insertEdgeMatrix(&gm1,0,5,8);
+   insertEdgeMatrix(&gm1,1,2,2);
+   insertEdgeMatrix(&gm1,1,3,4);
+   insertEdgeMatrix(&gm1,2,4,7);
+   insertEdgeMatrix(&gm1,2,5,4);
+   insertEdgeMatrix(&gm1,3,4,6);
+   insertEdgeMatrix(&gm1,4,6,3);
+   insertEdgeMatrix(&gm1,5,6,2);
+
+   showGraphMatrix(&gm1);
+
+   // LISTA DE ADYACENCIA
+
+   GraphList *gl1;
+
+   createGraphList(gl1);
+
+   gl1 = readGraphList(gl1);
+
+   // Monstrnado la lista vacia
+   // showGraphList(gl1);
+
+   if(gl1 != NULL)
+   {
+      insertEdgeList(gl1,0,1);
+      insertEdgeList(gl1,0,5);
+      insertEdgeList(gl1,1,2);
+      insertEdgeList(gl1,1,3);
+      insertEdgeList(gl1,2,4);
+      insertEdgeList(gl1,2,5);
+      insertEdgeList(gl1,3,4);
+      insertEdgeList(gl1,4,6);
+      insertEdgeList(gl1,5,6);
+
+      showGraphList(gl1);
+
+      printf("\nRecorrido DFS\n");
+
+      // Hacemos el recorrido DFS desde el vertice 0
+      DFS(gl1,0);
+
+      // Para comparar se prueba pasandole el vertice 1
+      // DFS(gl1,1);
+
+      freeGraphList(gl1);
+   }
+
+   return 0;
+}
+
+
+/*
+---> LABORATORIO DE GRAFOS
+
+Numero vertices Matriz Adyacencia: 5
+
+--- MATRIZ DE ADYACENCIA ---
+       0   1   2   3   4
+
+   0    0 INF INF INF INF
+   1  INF   0 INF INF INF
+   2  INF INF   0 INF INF
+   3  INF INF INF   0 INF
+   4  INF INF INF INF   0
+
+--- MATRIZ DE ADYACENCIA ---
+       0   1   2   3   4
+
+   0    0   5 INF INF   8
+   1  INF   0   2   6 INF
+   2  INF INF   0 INF   1
+   3  INF INF INF   0 INF
+   4  INF INF INF INF   0
+
+Numero vertices Lista Adyacencia:  6
+
+========================================
+ G R A F O S   Lista de Adyacencia
+----------------------------------------
+|  0|->  2-->  1--> NULL
+|  1|->  3--> NULL
+|  2|->  4--> NULL
+|  3|->  5--> NULL
+|  4|->  5--> NULL
+|  5|-> NULL
+----------------------------------------
+Recorrido en profunidad...
+
+Recorrido DFS desde vértice 0: 0 2 4 5 1 3
+
+Se libero mmemoria de grafo lista.
+
+Process returned 0 (0x0)   execution time : 7.151 s
+Press ENTER to continue.
+
+*/
